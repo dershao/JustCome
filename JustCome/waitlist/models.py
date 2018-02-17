1c45143fa9aa5a0f6e79eba92d985dcacda1ded9
gitoutplayed <mfan991@gmail.com>
2018-02-17 19:57:23 -0400

    commit

--- JustCome/waitlist/models.py
+++ JustCome/waitlist/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-#The patient model
-class Patient(models.Model):
-    patientID = models.CharField(max_length = 4)
-    device = models.CharField(max_length = 30)
-    priority = models.CharField(max_length = 30)
