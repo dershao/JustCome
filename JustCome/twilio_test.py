3ee36131822d71f22557ae1b5e90438e9e40c3e9
Derek Shao <shao.derek.now@gmail.com>
2018-02-17 19:56:32 -0500

    twilio

--- JustCome/twilio_test.py
+++ JustCome/twilio_test.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
-account_sid = AC7be8973e6f7a945d7707b14d220bb20c
+account_sid = "AC7be8973e6f7a945d7707b14d220bb20c"
 
-auth_token = effe642c95803d5907f0ae04aa53fb13
+auth_token = "effe642c95803d5907f0ae04aa53fb13"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     to="+16138181132",
-    from_="+15017250604",
+    from_="+18737388248",
     body="Hello!")
 
 print(message.sid)
