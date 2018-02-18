4e391ba2d717a6c3f5ddcaa377d4d05de5720d8f
gitoutplayed <mfan991@gmail.com>
2018-02-18 07:15:31 -0400

    merge

--- JustCome/waitlist/views.py
+++ JustCome/waitlist/views.py
@@ -4,6 +4,7 @@ from django.db import IntegrityError
 from .models import Patient
 from django.urls import reverse
 from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
 import json
 
 
@@ -22,16 +23,12 @@ def enqueue(request):
     #Create a new patient record
     patient = Patient(patientID=id, phoneNumber=number, priority=p)
     patient.save()
-<<<<<<< HEAD
 
-    return HttpResponse('success')
-=======
-    message = client.messages.create( 
-        to="+1"+number, 
-        from_="+18737388248", 
+    message = client.messages.create(
+        to="+1"+number,
+        from_="+18737388248",
         body="Thank you, you will be contacted when is is almost your turn.")
-    return HttpResponse("Success")
->>>>>>> 3aed7224664d50853932fd7fb7f5bce2a753d04b
+    return HttpResponse("success")
 
 def dequeue(request):
     #Get the information from the request
@@ -39,9 +36,9 @@ def dequeue(request):
 
     #Filter through the database via phone number (guaranteed to be unique)
     patient = Patient.Manager.filter(phoneNumber=number)
-    message = client.messages.create( 
-        to="+1" + patient[0].phoneNumber, 
-        from_="+18737388248", 
+    message = client.messages.create(
+        to="+1" + patient[0].phoneNumber,
+        from_="+18737388248",
         body="Someone is ready to see you now.")
     patient.delete()
 
@@ -53,3 +50,9 @@ def home(request):
     medium = Patient.Manager.filter(priority="medium")
     high = Patient.Manager.filter(priority="high")
     return render(request, "waitlist/DoctorMain.html", {"patient_low": low, "patient_medium":medium, "patient_high":high})
+
+def reply(request):
+    body = request.values.get('Body', None)
+    
+
+    return HttpResponse('success')
