76d86b3f06ec159037ce81b0fd77903a20a8f029
Christopher Wang <christopher.jj.wang@gmail.com>
2018-02-18 03:55:41 -0500

    Added phone number to model, changed dequeu, in progress change priority

--- JustCome/waitlist/static/waitlist/script.js
+++ JustCome/waitlist/static/waitlist/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+
+
   //THis is te code for enqueuing
   $("#submit").click(function() {
     var id = $("#phone").val();
@@ -20,35 +22,56 @@ $(document).ready(function() {
             },
           });
   });
-//This is the code for dequeuing
-  $("#next").click(function() {
-    $.ajax({
-            url: "/JustCome/waitlist/dequeue",
-            type: "GET",
-            success: function(data) {
-              alert(data);
-            },
-            failure: function(xhr) {
-              alert(xhr);
-            },
-          });
-  });
 
   $(".patient_button").click(function() {
     var dropdown = $(this).siblings('.dropdown-content');
     dropdown.toggle();
   });
 
-  window.onclick = function(event) {
-    if (!event.target.matches('.patient_button')) {
-      var dropdowns = document.getElementByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropDown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+  // window.onclick = function(event) {
+  //   if (!event.target.matches('.patient_button')) {
+  //     var dropdowns = document.getElementByClassName("dropdown-content");
+  //     var i;
+  //     for (i = 0; i < dropdowns.length; i++) {
+  //       var openDropDown = dropdowns[i];
+  //       if (openDropdown.classList.contains('show')) {
+  //         openDropdown.classList.remove('show');
+  //       }
+  //     }
+  //   }
+  // }
 });
+
+//Function to dequue a patient
+function dequeue(phoneNumber){
+  $.ajax({
+    url: "/JustCome/waitlist/dequeue",
+    data:{
+      phoneNumber : phoneNumber,
+    },
+    type: "GET",
+    success: function(data) {
+      location.reload(true);
+    },
+    failure: function(xhr) {
+      alert(xhr);
+    }
+  });
+}
+
+function movePrioritys(patientID){
+  $.ajax({
+    url: "/JustCome/waitlist/dequeue",
+    data:{
+      patientID : patientID,
+    },
+    type: "GET",
+    success: function(data) {
+      location.reload(true);
+    },
+    failure: function(xhr) {
+      alert(xhr);
+    }
+  });
+}
+
